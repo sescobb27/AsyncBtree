9aafa7e84ca43ac91e863bfc154d1fcf763492f6
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-05-02 15:09:44 -0500

    Refactoring and test passing

--- binarytree.go
+++ binarytree.go
@@ -1,121 +1,120 @@
 package binarytree
 
 type Obj interface {
-	Compare(node Obj) int
+        Compare(node Obj) int
 }
 
 type Tree struct {
-	Item        Obj
-	Rigth, Left *Tree
-	height      int16
+        Item        Obj
+        Rigth, Left *Tree
+        height      int16
 }
 
 func NewTree() *Tree {
-	return &Tree{Item: nil, Rigth: nil, Left: nil, height: 0}
+        return &Tree{Item: nil, Rigth: nil, Left: nil, height: 0}
 }
 
 func inOrder(t *Tree, chTree chan Obj) {
-	if t != nil {
-		inOrder(t.Left, chTree)
-		chTree <- t.Item
-		inOrder(t.Rigth, chTree)
-	}
+        if t != nil {
+                inOrder(t.Left, chTree)
+                chTree <- t.Item
+                inOrder(t.Rigth, chTree)
+        }
 }
 
 func InOrder(t *Tree, chTree chan Obj) {
-	inOrder(t, chTree)
-	close(chTree)
+        inOrder(t, chTree)
+        close(chTree)
 }
 
 func postOrder(t *Tree, chTree chan Obj) {
-	if t != nil {
-		postOrder(t.Left, chTree)
-		postOrder(t.Rigth, chTree)
-		chTree <- t.Item
-	}
+        if t != nil {
+                postOrder(t.Left, chTree)
+                postOrder(t.Rigth, chTree)
+                chTree <- t.Item
+        }
 }
 
 func PostOrder(t *Tree, chTree chan Obj) {
-	postOrder(t, chTree)
-	close(chTree)
+        postOrder(t, chTree)
+        close(chTree)
 }
 
 func preOrder(t *Tree, chTree chan Obj) {
-	if t != nil {
-		chTree <- t.Item
-		preOrder(t.Left, chTree)
-		preOrder(t.Rigth, chTree)
-	}
+        if t != nil {
+                chTree <- t.Item
+                preOrder(t.Left, chTree)
+                preOrder(t.Rigth, chTree)
+        }
 }
 
 func PreOrder(t *Tree, chTree chan Obj) {
-	preOrder(t, chTree)
-	close(chTree)
+        preOrder(t, chTree)
+        close(chTree)
 }
 
 func insert(t *Tree, item Obj, done chan struct{}) {
-	if t.Item == nil {
-		t.Item = item
-		var signal struct{}
-		done <- signal
-		return
-	}
-	if t.Item.Compare(item) == 1 { //Left
-		if t.Left == nil {
-			t.Left = &Tree{Item: item,
-				Rigth: nil,
-				Left:  nil,
-			}
-			var signal struct{}
-			done <- signal
-		} else {
-			insert(t.Left, item, done)
-			return
-		}
-	} else if t.Item.Compare(item) == -1 { //Rigth
-		if t.Rigth == nil {
-			t.Rigth = &Tree{Item: item,
-				Rigth: nil,
-				Left:  nil,
-			}
-			var signal struct{}
-			done <- signal
-		} else {
-			insert(t.Rigth, item, done)
-			return
-		}
-	}
-	close(done)
-	return //errors.New("Item already exist")
+        if t.Item == nil {
+                t.Item = item
+                var signal struct{}
+                done <- signal
+        } else {
+                if t.Item.Compare(item) == 1 { //Left
+                        if t.Left == nil {
+                                t.Left = &Tree{Item: item,
+                                        Rigth:  nil,
+                                        Left:   nil,
+                                }
+                                var signal struct{}
+                                done <- signal
+                        } else {
+                                insert(t.Left, item, done)
+                        }
+                } else if t.Item.Compare(item) == -1 { //Rigth
+                        if t.Rigth == nil {
+                                t.Rigth = &Tree{Item: item,
+                                        Rigth:  nil,
+                                        Left:   nil,
+                                }
+                                var signal struct{}
+                                done <- signal
+                        } else {
+                                insert(t.Rigth, item, done)
+                        }
+                } else {
+                        close(done)
+                }
+        }
 }
 
 func Insert(t *Tree, item Obj) chan struct{} {
-	done := make(chan struct{}, 1)
-	go insert(t, item, done)
-	return done
+        done := make(chan struct{}, 1)
+        go insert(t, item, done)
+        return done
 }
 
 func (t *Tree) Delete(item Obj) error {
-	// TO-DO
-	return nil
+        // TO-DO
+        return nil
 }
 
 func find(t *Tree, item Obj, ch_result chan Tree) {
-	if t != nil {
-		if t.Item.Compare(item) == 0 {
-			ch_result <- *t
-			return
-		} else if t.Item.Compare(item) == -1 { //Rigth
-			Find(t.Rigth, item, ch_result)
-			return
-		} else { //Left
-			Find(t.Left, item, ch_result)
-			return
-		}
-	}
-	ch_result <- *NewTree()
+        if t != nil {
+                if t.Item.Compare(item) == 0 {
+                        ch_result <- *t
+                        close(ch_result)
+                } else if t.Item.Compare(item) == -1 { //Rigth
+                        find(t.Rigth, item, ch_result)
+                } else { //Left
+                        find(t.Left, item, ch_result)
+                }
+        } else {
+                close(ch_result)
+        }
 }
 
-func Find(t *Tree, item Obj, ch_result chan Tree) {
-	find(t, item, ch_result)
+func Find(t *Tree, item Obj) chan Tree {
+        ch_result := make(chan Tree, 1)
+        go find(t, item, ch_result)
+        return ch_result
 }
